ea7232257f1a3ebbbb53e35382ce3908e2992e8e
Paul_Mirzoyants <pavcnfh@yandex.ru>
2025-03-21 14:16:57 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+
+import './scss/app.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div
+          className="content"
+          style={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              height: '100vh',
+          }}
+      >
+          <div
+              className="malevich-square"
+              style={{
+                  width: '40vh',
+                  height: '40vh',
+                  backgroundColor: 'black',
+              }}
+          ></div>
+      </div>
   );
 }
 
