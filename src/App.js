835d6a9d40defb91916bc322dd82939eda4d63d2
Paul_Mirzoyants <pavcnfh@yandex.ru>
2025-03-21 15:15:56 +0300

    Small fix

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 
-import './scss/app.scss';
-
 function App() {
+
+	const contentStyle = {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+    };
+
+	  const squareStyle = {
+          width: '40vh',
+          height: '40vh',
+          backgroundColor: 'black',
+      };
+
   return (
-      <div
-          className="content"
-          style={{
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-              height: '100vh',
-          }}
-      >
-          <div
-              className="malevich-square"
-              style={{
-                  width: '40vh',
-                  height: '40vh',
-                  backgroundColor: 'black',
-              }}
-          ></div>
+      <div className="content" style={contentStyle}>
+          <div className="malevich-square" style={squareStyle}></div>
       </div>
   );
 }
